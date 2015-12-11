72c8a57c681c70b658b21da70a9ae8027c513daa
AtlantaBukkit <atlanta.bukkit@gmail.com>
2015-12-11 21:53:39 +0100

    Optimized ItemUtils class and added convenience methods

--- src/main/java/com/github/atlantabukkit/mcze/utils/ItemUtils.java
+++ src/main/java/com/github/atlantabukkit/mcze/utils/ItemUtils.java
@@ -8,12 +8,23 @@ import java.util.Arrays;
 
 public class ItemUtils {
 
-    public static ItemStack createItem(Material material, String name, String[] lore, int amount, short data) {
+    public static ItemStack createItem(Material material, String name, String... lore) {
+        return createItem(material, name, 1, lore);
+    }
+
+    public static ItemStack createItem(Material material, String name, int amount, String... lore) {
+        return createItem(material, name, amount, (short) 0, lore);
+    }
+    public static ItemStack createItem(Material material, String name, int amount, short data, String... lore) {
         ItemStack item = new ItemStack(material, amount, data);
         ItemMeta meta = item.getItemMeta();
 
-        meta.setDisplayName(name);
-        meta.setLore(Arrays.asList(lore));
+        if (name != null) {
+            meta.setDisplayName(name);
+        }
+        if (lore != null && lore.length > 0) {
+            meta.setLore(Arrays.asList(lore));
+        }
 
         item.setItemMeta(meta);
         return item;
